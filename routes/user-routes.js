6be6f5a46e08b2f39159e033f06704a6906146da
Thet Htar Nwe <iamthethtarnwe@gmail.com>
2026-03-20 21:43:23 +0800

    add login and forgot password feature

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -9,4 +9,13 @@ router.get("/profile", userController.showProfilePage);
 router.get("/change-password", userController.showChangePasswordPage);
 router.post("/change-password", userController.changePassword);
 
+router.get("/login", userController.showLoginForm);
+router.post("/login", userController.loginUser);
+router.post("/logout", userController.logoutUser);
+
+router.get("/forgot-password", userController.showForgotPasswordForm);
+router.post("/forgot-password", userController.verifyForgotPassword);
+router.get("/reset-password/:id", userController.showResetPasswordForm);
+router.post("/reset-password/:id", userController.resetPassword);
+
 module.exports = router;
